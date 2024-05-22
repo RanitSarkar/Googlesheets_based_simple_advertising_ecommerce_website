96e7a830902d86d8a5c282b88bbb2f8a7246effb
RANIT SARKAR <85814394+RANIT001@users.noreply.github.com>
2024-05-22 08:29:48 +0530

    Merge remote-tracking branch 'origin/master'

--- commerce_client_side/main.py
+++ commerce_client_side/main.py
@@ -6,4 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("main.html")
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1',port=5000,debug=True)
