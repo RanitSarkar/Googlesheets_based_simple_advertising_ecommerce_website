3c6da5e8db355470f30190969cd4e711c785d310
RANIT SARKAR <85814394+RANIT001@users.noreply.github.com>
2024-05-22 07:43:24 +0530

    Initial commit

---
+++ commerce_client_side/main.py
@@ -0,0 +1,8 @@
+import firebase_admin
+from firebase_admin import firestore
+
+# Application Default credentials are automatically created.
+app = firebase_admin.initialize_app()
+db = firestore.client()
+
+
