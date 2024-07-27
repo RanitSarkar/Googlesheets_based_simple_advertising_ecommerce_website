bac0789957298c5ebe7740a67870a862f9f04059
Ranit_Sarkar <ranitsarkar71@gmail.com>
2024-07-27 21:05:15 +0530

    Merge remote-tracking branch 'origin/master'

--- E_Commerce_website/main.py
+++ E_Commerce_website/main.py
@@ -12,26 +12,26 @@ def home():
     return render_template("index.html",)
 
 
-@app.route("/signup_page.html", methods=['GET', 'POST'])
-def signup_page():
-    global otp
-    my_email = "codewithmrpy@gmail.com"
-    password = "**********"
-    if request.method == "POST":
-        otp = random.randint(519166, 8451653)
-        email = request.form['email']
-        # password = request.form['password']
-        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-            connection.starttls()
-            connection.login(user=my_email, password=password)
-            connection.sendmail(
-                from_addr=my_email,
-                to_addrs=email,
-                msg=f"subject:Your signup OTP \n\n {otp}")
-        return redirect(url_for('OTP_a', email=email, otp=otp))
-    else:
-        pass
-    return render_template("signup_page.html")
+# @app.route("/signup_page.html", methods=['GET', 'POST'])
+# def signup_page():
+#     global otp
+#     my_email = "codewithmrpy@gmail.com"
+#     password = "**********"
+#     if request.method == "POST":
+#         otp = random.randint(519166, 8451653)
+#         email = request.form['email']
+#         # password = request.form['password']
+#         with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+#             connection.starttls()
+#             connection.login(user=my_email, password=password)
+#             connection.sendmail(
+#                 from_addr=my_email,
+#                 to_addrs=email,
+#                 msg=f"subject:Your signup OTP \n\n {otp}")
+#         return redirect(url_for('OTP_a', email=email, otp=otp))
+#     else:
+#         pass
+#     return render_template("signup_page.html")
 
 
 if __name__ == "__main__":
