744368dc06e763f689616858c233b98fbe449b67
Ranit_Sarkar <ranitsarkar71@gmail.com>
2024-08-26 08:54:37 +0530

    Merge remote-tracking branch 'origin/master'

--- E_Commerce_website/main.py
+++ E_Commerce_website/main.py
@@ -1,38 +1,25 @@
-from flask import Flask, render_template, redirect, jsonify, request, url_for
-import random
-import smtplib
-import datetime as dt
+from flask import Flask, render_template
 import requests
+import csv
+from io import StringIO
 
 app = Flask(__name__)
 
+# Google Sheet CSV link
+CSV_URL = "https://docs.google.com/spreadsheets/d/1e7dBXSF0WwFqfJOxs4bzEVbmXZbVrLwl4ASs4o9kCvM/pub?gid=0&single=true&output=csv"
 
-@app.route("/")
-def home():
-    return render_template("index.html",)
+def fetch_csv_data(url):
+    response = requests.get(url)
+    csv_text = response.text
+    f = StringIO(csv_text)
+    reader = csv.DictReader(f, delimiter=',')
+    data = [row for row in reader]
+    return data
 
+@app.route('/')
+def index():
+    products = fetch_csv_data(CSV_URL)
+    return render_template('index.html', products=products)
 
-# @app.route("/signup_page.html", methods=['GET', 'POST'])
-# def signup_page():
-#     global otp
-#     my_email = "codewithmrpy@gmail.com"
-#     password = "**********"
-#     if request.method == "POST":
-#         otp = random.randint(519166, 8451653)
-#         email = request.form['email']
-#         # password = request.form['password']
-#         with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-#             connection.starttls()
-#             connection.login(user=my_email, password=password)
-#             connection.sendmail(
-#                 from_addr=my_email,
-#                 to_addrs=email,
-#                 msg=f"subject:Your signup OTP \n\n {otp}")
-#         return redirect(url_for('OTP_a', email=email, otp=otp))
-#     else:
-#         pass
-#     return render_template("signup_page.html")
-
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=5000,debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
